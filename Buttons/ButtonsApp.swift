b80dc6cc651b12c4b536b4aab38b219c9b010b78
Yogesh Bhusara <yogesh.bhusara@outlook.com>
2021-05-28 12:37:56 +0530

    Initial Commit

---
+++ Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by Yogesh Bhusara on 28/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buttons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buttons
+//
+//  Created by Yogesh Bhusara on 28/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
